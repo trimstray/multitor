52a876fb966d95e1539db0f97234049cd24827ed
Jan Seidl <jseidl@wroot.org>
2012-08-31 18:16:38 -0300

    Added shellscript

---
+++ multi-tor.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+base_socks_port=9050
+base_control_port=8118
+
+# Create data directory if it doesn't exist
+if [ ! -d "data" ]; then
+	mkdir "data"
+fi
+
+TOR_INSTANCES="$1"
+
+if [ ! $TOR_INSTANCES ] || [ $TOR_INSTANCES -lt 1 ]; then
+    echo "Please supply an instance count"
+    echo "Example: ./multi-tor.sh 5"
+    exit 1
+fi
+
+#for i in {0..10}
+for i in $(seq $TOR_INSTANCES)
+
+do
+	j=$((i+1))
+	socks_port=$((base_socks_port+i))
+	control_port=$((base_control_port+i))
+	if [ ! -d "data/tor$i" ]; then
+		echo "Creating directory data/tor$i"
+		mkdir "data/tor$i"
+	fi
+	# Take into account that authentication for the control port is disabled. Must be used in secure and controlled environments
+
+	echo "Running: tor --RunAsDaemon 1 --CookieAuthentication 0 --HashedControlPassword \"\" --ControlPort $control_port --PidFile tor$i.pid --SocksPort $socks_port --DataDirectory data/tor$i"
+
+	tor --RunAsDaemon 1 --CookieAuthentication 0 --HashedControlPassword "" --ControlPort $control_port --PidFile tor$i.pid --SocksPort $socks_port --DataDirectory data/tor$i
+done
